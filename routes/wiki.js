198e514c4ca9afd72d208599aa5b565b8c5b8c33
Matthew Contract <mcontract27@gmail.com>
2018-04-27 13:02:31 -0400

    post implemented

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const addPage = require('../views/addPage')
+const {Page} = require('../models')
 
 router.get('/', (req, res) =>{
     res.redirect('/');
 })
 
-router.post('/', (req, res) => {
-    res.json(req.body);
+router.post('/', async (req, res, next) => {
+    const page = new Page({
+        title: req.body.title,
+        content: req.body.content,
+        status: req.body.status,
+        slug: req.body.title.replace(/\s+/g, "_").replace(/[^\w\-]+/g, '')
+    })
+
+    try {
+        await page.save();
+        res.redirect('/');
+    } catch (error) { next(error) }
 })
+
 
 router.get('/add', (req, res) =>{
     res.send(addPage());
