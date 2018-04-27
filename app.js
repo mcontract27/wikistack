cf5f091faf910c3aa2b64b65baec1027eebe99f3
Matthew Contract <mcontract27@gmail.com>
2018-04-27 11:32:34 -0400

    removed node_modules

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const layout = require('./views/layout'); 
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send(layout('Hello world!'));
+});
+
+app.listen(3000);
